4bfaba107747d1b2f89da4d53dd999cb6abb8c9b
Sebastián Aedo <sebastian.aedo29@gmail.com>
2023-02-28 11:06:08 -0300

    Removed default comment

--- src/pastenim.nim
+++ src/pastenim.nim
@@ -1,7 +1,4 @@
 include pastenim/internal
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
 
 proc clipboard_new*(): Clipboard =
   let status = clipboard_new(result.addr)
